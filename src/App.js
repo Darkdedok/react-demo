f96351bef9504b19f875f8b710199740cdae264d
Darkdedok <darkdedok@gmail.com>
2022-06-21 17:16:20 +0300

    Finish HM_1

--- src/App.js
+++ src/App.js
@@ -1,64 +1,29 @@
 import './App.css';
-import RickAndMortyCharacter from "./Characters/RickAndMorty/RickAndMorty";
+import SimpsonCharacter from "./Characters/Simpsons/Simpsons";
 function App(props) {
     return (
         <div>
-            <RickAndMortyCharacter
-id = {'1'}
-name = {'Rick Sanchez'}
-status = {'Alive'}
-species = {'Human'}
-gender = {'Male'}
-image = {'https://rickandmortyapi.com/api/character/avatar/1.jpeg'}
+            <SimpsonCharacter name={'Bart'}
+                              biography={'Bart is a self-proclaimed underachiever and prankster who is constantly in detention who in addition to his behavior is easily distracted. '}
+                              picture={'https://static.wikia.nocookie.net/simpsons/images/6/65/Bart_Simpson.png'}
             />
-            <br/>
-                        <RickAndMortyCharacter
-id = {'2'}
-name = {'Morty Smith'}
-status = {'Alive'}
-species = {'Human'}
-gender = {'Male'}
-image = {'https://rickandmortyapi.com/api/character/avatar/2.jpeg'}
+            <SimpsonCharacter name={'Lisa Simpson'}
+                              biography={'Lisa is quite eclectic in her knowledge and is notably more concerned with world affairs and problems than her cohorts, which has led her to alienate herself from her peers.'}
+                              picture={'https://static.wikia.nocookie.net/simpsons/images/d/d5/Lisa_Simpson_official.png'}
             />
-            <br/>
-                        <RickAndMortyCharacter
-id = {'3'}
-name = {'Summer Smith'}
-status = {'Alive'}
-species = {'Human'}
-gender = {'Female'}
-image = {'https://rickandmortyapi.com/api/character/avatar/3.jpeg'}
+            <SimpsonCharacter name={'Maggie Simpson'}
+                              biography={'When Marge became pregnant with Lisa, she and Homer bought their first home. Seven years later, Homer felt financially secure enough to quit his job at the power plant and take his dream job at Barney\'s Bowlarama.'}
+                              picture={'https://static.wikia.nocookie.net/simpsons/images/9/9d/Maggie_Simpson.png'}
             />
-            <br/>
-                        <RickAndMortyCharacter
-id = {'4'}
-name = {'Beth Smith'}
-status = {'Alive'}
-species = {'Human'}
-gender = {'Female'}
-image = {'https://rickandmortyapi.com/api/character/avatar/4.jpeg'}
+            <SimpsonCharacter name={'Marge Simpson'}
+                              biography={'Marge Bouvier was born on in Capital City to Clancy Bouvier, a photographer-turned-steward, and Jacqueline Bouvier.'}
+                              picture={'https://static.wikia.nocookie.net/simpsons/images/4/4d/MargeSimpson.png'}
             />
-            <br/>
-                        <RickAndMortyCharacter
-id = {'5'}
-name = {'Jerry Smith'}
-status = {'Alive'}
-species = {'Human'}
-gender = {'Male'}
-image = {'https://rickandmortyapi.com/api/character/avatar/5.jpeg'}
+            <SimpsonCharacter name={'Homer Simpson'}
+                              biography={'Homer Jay Simpson was born on May 12, 1956 to Abraham Simpson II and Mona Olsen. He was raised on the Simpson family farm until they were forced to move out due to Homer jumping out of a hay bale and scaring the cows into giving sour milk, causing the bank to foreclose it.'}
+                              picture={'https://static.wikia.nocookie.net/simpsons/images/b/bd/Homer_Simpson.png'}
             />
-            <br/>
-                        <RickAndMortyCharacter
-id = {'6'}
-name = {'Abadango Cluster Princess'}
-status = {'Alive'}
-species = {'Alien'}
-gender = {'Female'}
-image = {'https://rickandmortyapi.com/api/character/avatar/6.jpeg'}
-            />
-
         </div>
     );
 }
-
 export default App;
